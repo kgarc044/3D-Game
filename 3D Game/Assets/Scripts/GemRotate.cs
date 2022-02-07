559cf842a5ac4682a6d1464045a2c36e3592ccb8
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-02-06 21:06:05 -0800

    Added more terrain attributes
    Trees and foilage have been added, along with a rotating gem

---
+++ 3D Game/Assets/Scripts/GemRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int RotateSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotateSpeed, 0, Space.World);
+    }
+}
