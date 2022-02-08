a0f4c62fce578a1c1f4f90b5d59b474805349fbb
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-02-07 16:44:31 -0800

    Axe animation

---
+++ 3D Game/Assets/Scripts/AxeSwing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSwing : MonoBehaviour
+{
+
+    public GameObject theAxe;
+    public bool isSwinging = false;
+    public AudioSource swingSound;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && isSwinging == false)
+        {
+            isSwinging = true;
+            StartCoroutine(SwingTheAxe());
+        }
+
+    }
+
+    IEnumerator SwingTheAxe()
+    {
+        swingSound.Play();
+        theAxe.GetComponent<Animation>().Play("SwingAxe");
+        yield return new WaitForSeconds(0.55f);
+        isSwinging = false;
+    }
+}
